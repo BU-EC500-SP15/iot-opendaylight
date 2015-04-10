7ee41c4e9fcb1a2fdc52c9903fff56442d19cc6d
odedranh <odedranh@bu.edu>
2015-04-10 13:09:18 -0400

    Signed-off-by: odedranh <odedranh@bu.edu>

---
+++ additional scripts for Tree/parsing exp.js
@@ -0,0 +1,109 @@
+﻿var http = require('http');
+var postResponse =
+  '<html><head><title>ODl For Post</title></head>' +
+  '<body>' +
+  '<form method="post">' +
+  'POST Recieved: <input name="postmsg"><br>'+
+  '</form>' +
+  '</body></html>';
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var body = "";
+    req.on('data', function (dt) {
+        body += dt;
+    });
+    req.on('end', function () {
+       res.writeHead(200);  
+      //var decodedText = decodeURIComponent(body);
+        console.log('POST made: ' + body);
+        var store = JSON.parse(body);
+        var str2 = JSON.stringify(store.output);
+        var str3 = JSON.stringify(store.output.ResourceOutput[0]["attributeValue"]);
+        var wstream = fs.createWriteStream('json1.json');
+        wstream.write(str2 + '\nJSONObject');
+        wstream.write('---------\n'+str3);
+        wstream.end();
+        res.end(postResponse);
+    });
+
+
+}).listen(8111);
+/* NodeJS Server
+var http = require('http');
+var API = "http://admin:admin@52.10.62.166:8282/InCSE1/LocationAE/Things?requestIdentifier=12345&Content-Type=application/json&Accept=application/json&from=http:localhost:10000/callback=foo";
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response)
+{
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+ 
+});
+     
+ 
+var http = require('http');
+var postHTML = 
+  '<html><head><title>Post Example</title></head>' +
+  '<body>' +
+  '<form method="post">' +
+  'Input 1: <input name="input1"><br>' +
+  'Input 2: <input name="input2"><br>' +
+  '<input type="submit">' +
+  '</form>' +
+  '</body></html>';
+
+http.createServer(function (req, res) {
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        console.log('POSTed: ' + body);
+        res.writeHead(200);
+        res.end(postHTML);
+    });
+}).listen(8080);
+<!--
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="id01"></div>
+
+<script>
+    var xmlhttp = new XMLHttpRequest();
+var url = "http://admin:admin@52.10.62.166:8282/InCSE1/LocationAE?requestIdentifier=12345&Content-Type=application/json&Accept=application/json&from=http:localhost:10000/callback=foo";
+
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' +
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("id01").innerHTML = out;
+}
+</script>
+
+</body>
+</html>-->
+*/
---
+++ additional scripts for Tree/pull a resource using console.js
@@ -0,0 +1,48 @@
+﻿var http = require('http');
+
+//http://52.10.62.166:8282/InCSE1/LocationAE?from=http:52.10.62.166:10000&requestIdentifier=12345&Content-Type=application/json&Accept=application/json
+var xpath,store,str2;
+process.argv.forEach(function (val, index, array) {
+        if (index < 2) {
+        }
+        else {
+            console.log(index + ': ' + val);
+            xpath = val
+        }
+    });
+var options = {
+    
+  host: '52.10.62.166',
+  port: 8282,
+  path: '/InCSE1/'+xpath+'?from=http:52.10.62.166:10000&requestIdentifier=12345&Content-Type=application/json&Accept=application/json'  
+//path: '/InCSE1/LocationAE?from=http:52.10.62.166:10000&requestIdentifier=12345&Content-Type=application/json&Accept=application/json'
+};
+callback = function(response) {
+  var str = '';
+
+  response.on('data', function (dt) {
+    str += dt;   
+    store = JSON.parse(str);
+    str2 = JSON.stringify(store.output)
+  });
+
+  response.on('end', function () {
+    console.log(str);
+    console.log("-----------------Here is the PARSED OUTPUT for "+xpath+" :------------");
+    console.log(store);
+    console.log('\n'+"-----------------Response Status :------------");
+    console.log(JSON.stringify(store.output.responseStatusCode)); 
+    console.log('\n'+"-----------------Resource Output :------------");   
+    console.log('\n'+"1-----Resource Type :------------");
+   console.log(JSON.stringify(store.output.ResourceOutput[0]["resourceType"]));
+   console.log('\n'+"2-----Resource ID :------------");
+   console.log(JSON.stringify(store.output.ResourceOutput[0]["resourceID"]));
+   console.log('\n'+"3-----Resource Attributes :------------");
+   console.log(JSON.stringify(store.output.ResourceOutput[0]["Attributes"]));
+   console.log('\n' + "3-----Resource AttributesNNNNNN :------------");
+
+   console.log(JSON.stringify(store.output.ResourceOutput[0].Attributes[0]));
+
+  });
+}
+http.request(options, callback).end();
